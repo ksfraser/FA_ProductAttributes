d7bcff26116f2a243f1d147de472e773f7cbf8e6
Kevin Fraser <kevin@ksfraser.ca>
2026-01-21 02:24:08 -0700

    Fix display_notification() called before UI includes loaded
    - Moved display_notification() calls to after ui.inc is included
    - Removed the early debug call that was causing the fatal error
    - display_notification() function is now available when called

--- product_attributes_admin.php
+++ product_attributes_admin.php
@@ -1,7 +1,6 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-display_notification("DEBUG: Error reporting enabled");
 
 // FrontAccounting wrapper admin page.
 // Place this repo under FA: modules/FA_ProductAttributes
